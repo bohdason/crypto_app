b479430ae37148a008540f29f83b5e6aa767a541
Bohdan Herasimchuk <bogdan.zaytsev.08@gmail.com>
2023-03-17 15:42:12 +0200

    first commit

---
+++ style.js
@@ -0,0 +1,42 @@
+
+async function showCurencies() {
+  try {
+    const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Ctether%2Cethereum%2Clitecoin%2Ccardano%2Cdogecoin&vs_currencies=usd&include_24hr_change=true');
+    const json = await response.json();
+    const container = document.querySelector('.container');
+    const coins = Object.getOwnPropertyNames(json);
+    console.log(coins);
+
+    for (let coin of coins) {
+      const coinInfo = json[`${coin}`];
+      console.log(coinInfo);
+      const price = coinInfo.usd;
+      const change = coinInfo.usd_24h_change.toFixed(5);
+
+      container.innerHTML += `
+            <div class="coin ${change < 0 ? 'falling' : 'rising'}">
+            <div class="common_part">
+            <div class="coin-logo">
+              <img src="images/${coin}.png">
+            </div>
+            <div class="coin-name">
+              <h3>${coin}</h3>
+              <span>/USD</span>
+            </div>
+            </div>
+            <div class="coin-price">
+              <span class="price">$${price}</span>
+              <span class="change">${change}</span>
+            </div>
+          </div>
+            `;
+    }
+    const button = document.getElementById('show_btn');
+    button.remove();
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+document.getElementById('show_btn').addEventListener("click", showCurencies);
+
